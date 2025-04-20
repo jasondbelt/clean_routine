c9180e8d11ee42b56f34117a6c4bc3d1e71abaff
Jason Belt <jasondbelt@gmail.com>
2025-04-20 16:43:29 -0500

    redoing tasks

--- back-end/task_app/urls.py
+++ back-end/task_app/urls.py
@@ -1,10 +1,11 @@
 #Task_APP.URLS
 from django.urls import path
-from .views import All_tasks, Tasks_by_day, CR_all_room_tasks, RUD_room_tasks
+from .views import Tasks_by_day, All_tasks
+# , Tasks_by_day, CR_all_room_tasks, RUD_room_tasks
 
 urlpatterns = [
-    path('', All_tasks.as_view(), name='all_tasks'),
     path('day/<str:day>/', Tasks_by_day, name='tasks_by_day'),
-    path('room_id/<int:room_id>/', CR_all_room_tasks.as_view(), name='cr_all_room_tasks'),
-    path('room_id/<int:room_id>/task_id/<int:task_id>/', RUD_room_tasks.as_view(), name='ud_room_tasks'),
+    path('', All_tasks, name='all_tasks'),
+    # path('room_id/<int:room_id>/', CR_all_room_tasks.as_view(), name='cr_all_room_tasks'),
+    # path('room_id/<int:room_id>/task_id/<int:task_id>/', RUD_room_tasks.as_view(), name='ud_room_tasks'),
 ]
--- back-end/task_app/views.py
+++ back-end/task_app/views.py
@@ -22,90 +22,88 @@ def Tasks_by_day(request, day):
     tasks_serializer = DayTasksSerializer(tasks, many=True)
     return Response(tasks_serializer.data)
 
-# view all tasks
-class All_tasks(APIView):
-    
-    # define custom ordering of days of the week
-    @staticmethod
-    def day_order():
-        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ]
-        return Case(
-        *[When(day_of_week=day, then=i) for i, day in enumerate(days)],
-        output_field=IntegerField()
-    )
 
-    # retrieve all tasks
-    def get(self, request):
-        # filter by user and order by day of week and time of day
-        tasks = Task.objects.filter(user=request.user).order_by(self.day_order(), 'time_of_day')
-        # serialize and return as a response
-        tasks_ser = AllTasksSerializer(tasks, many=True)
-        return Response(tasks_ser.data)
+# define custom ordering of days of the week
+@staticmethod
+def day_order():
+    days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ]
+    return Case(
+    *[When(day_of_week=day, then=i) for i, day in enumerate(days)],
+    output_field=IntegerField()
+)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def All_tasks(request):
+    tasks = Task.objects.filter(user=request.user).order_by(day_order(), 'time_of_day')
+    # serialize and return as a response
+    tasks_ser = AllTasksSerializer(tasks, many=True)
+    return Response(tasks_ser.data)
     
 
-# had to use decorator and permission class to be able to authenticate get on front-end
-# filter tasks by down by user and day of week and returned in serializer response
-# and format it it
+# # had to use decorator and permission class to be able to authenticate get on front-end
+# # filter tasks by down by user and day of week and returned in serializer response
+# # and format it it
 
 
 
-# view all tasks within a specific room, post new tasks
-class CR_all_room_tasks(APIView):
+# # view all tasks within a specific room, post new tasks
+# class CR_all_room_tasks(APIView):
 
-    @staticmethod
-    def day_order():
-        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ]
-        return Case(
-        *[When(day_of_week=day, then=i) for i, day in enumerate(days)],
-        output_field=IntegerField()
-    )
+#     @staticmethod
+#     def day_order():
+#         days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ]
+#         return Case(
+#         *[When(day_of_week=day, then=i) for i, day in enumerate(days)],
+#         output_field=IntegerField()
+#     )
 
-    # retrieve all tasks within a specfic room
-    def get(self, request, room_id):
-        # filter by room and user, sort order by day of week and time of day
-        room_tasks = Task.objects.filter(room=room_id, user=request.user).order_by(self.day_order(), 'time_of_day')
-        # serialize and return as a response
-        ser_room_tasks = TaskSerializer(room_tasks, many=True)
-        return Response(ser_room_tasks.data, status=HTTP_200_OK)
+#     # retrieve all tasks within a specfic room
+#     def get(self, request, room_id):
+#         # filter by room and user, sort order by day of week and time of day
+#         room_tasks = Task.objects.filter(room=room_id, user=request.user).order_by(self.day_order(), 'time_of_day')
+#         # serialize and return as a response
+#         ser_room_tasks = TaskSerializer(room_tasks, many=True)
+#         return Response(ser_room_tasks.data, status=HTTP_200_OK)
     
-    # create a new task in a specific room
-    def post(self, request, room_id):
-        data = request.data.copy()
-        # add room_id to data
-        data['room'] = room_id
-        # serialize the data
-        task_ser = TaskSerializer(data=data)
-        # if data is valid, save and return serialized response
-        if task_ser.is_valid():
-            task_ser.save(user=request.user, room_id=room_id)
-            return Response(task_ser.data, status=HTTP_201_CREATED)
-        return Response(task_ser.errors, status=HTTP_400_BAD_REQUEST)
+#     # create a new task in a specific room
+#     def post(self, request, room_id):
+#         data = request.data.copy()
+#         # add room_id to data
+#         data['room'] = room_id
+#         # serialize the data
+#         task_ser = TaskSerializer(data=data)
+#         # if data is valid, save and return serialized response
+#         if task_ser.is_valid():
+#             task_ser.save(user=request.user, room_id=room_id)
+#             return Response(task_ser.data, status=HTTP_201_CREATED)
+#         return Response(task_ser.errors, status=HTTP_400_BAD_REQUEST)
     
 
-# retrieve, update, and delete tasks within a specific room
-class RUD_room_tasks(APIView):
+# # retrieve, update, and delete tasks within a specific room
+# class RUD_room_tasks(APIView):
 
-    # retrive a specific task by id within a specific room
-    def get(self, request, room_id, task_id):
-        task = get_object_or_404(Task, id=task_id, room=room_id, user=request.user)
-        ser_room_task = TaskSerializer(task)
-        return Response(ser_room_task.data, status=HTTP_200_OK)
+#     # retrive a specific task by id within a specific room
+#     def get(self, request, room_id, task_id):
+#         task = get_object_or_404(Task, id=task_id, room=room_id, user=request.user)
+#         ser_room_task = TaskSerializer(task)
+#         return Response(ser_room_task.data, status=HTTP_200_OK)
     
-    # update the specific task
-    def put(self, request, room_id, task_id):
-        data = request.data.copy()
-        task = get_object_or_404(Task, id=task_id, room=room_id, user=request.user)
-        task_ser = TaskSerializer(task, data=data, partial=True)
+#     # update the specific task
+#     def put(self, request, room_id, task_id):
+#         data = request.data.copy()
+#         task = get_object_or_404(Task, id=task_id, room=room_id, user=request.user)
+#         task_ser = TaskSerializer(task, data=data, partial=True)
         
-        # if data is valid, save and return success response
-        if task_ser.is_valid():
-            task_ser.save()
-            return Response(task_ser.data, status=HTTP_201_CREATED)
+#         # if data is valid, save and return success response
+#         if task_ser.is_valid():
+#             task_ser.save()
+#             return Response(task_ser.data, status=HTTP_201_CREATED)
 
-        return Response(task_ser.errors, status=HTTP_400_BAD_REQUEST)
+#         return Response(task_ser.errors, status=HTTP_400_BAD_REQUEST)
 
-    # delete the specific task utilizing room and task ids
-    def delete(self, request, room_id, task_id):
-        task = get_object_or_404(Task, id=task_id, room=room_id, user=request.user)
-        task.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+#     # delete the specific task utilizing room and task ids
+#     def delete(self, request, room_id, task_id):
+#         task = get_object_or_404(Task, id=task_id, room=room_id, user=request.user)
+#         task.delete()
+#         return Response(status=HTTP_204_NO_CONTENT)
